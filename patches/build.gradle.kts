1a5d043c220ccb16507b3c76130b590760c32fbc
Bartosz Wróblewski <b@wr.wtf>
2025-11-05 19:26:40 -0800

    Prep.

--- patches/build.gradle.kts
+++ patches/build.gradle.kts
@@ -1,12 +1,12 @@
-group = "app.revanced"
+group = "app.bawr.revanced"
 
 patches {
     about {
-        name = "ReVanced Patches template"
-        description = "Patches template for ReVanced"
-        source = "git@github.com:revanced/revanced-patches-template.git"
-        author = "ReVanced"
-        contact = "contact@revanced.app"
+        name = "ReVanced Patches"
+        description = ""
+        source = "git@github.com:bawr/revanced-patches.git"
+        author = "bawr"
+        contact = "don't"
         website = "https://revanced.app"
         license = "GNU General Public License v3.0"
     }
